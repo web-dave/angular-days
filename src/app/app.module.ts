78f2949ebddea4c495841a3793b82ff0cd979d5b
web-dave <dave@webdave.de>
2022-03-22 11:35:34 +0100

    top-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SwapiModule } from './swapi/swapi.module';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TopNavComponent],
   imports: [BrowserModule, AppRoutingModule, SwapiModule],
   providers: [],
   bootstrap: [AppComponent],
