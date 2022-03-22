840572776dbea856e16b557f6ed8753ee9f3a368
web-dave <dave@webdave.de>
2022-03-22 17:11:42 +0100

    switchmap

--- src/app/swapi/film/film.component.ts
+++ src/app/swapi/film/film.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { ApiService, IFilm } from '../api.service';
 
 @Component({
@@ -17,6 +17,16 @@ export class FilmComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.route.params.subscribe((data) =>
+      this.api
+        .getEntity('films', data['id'])
+        .subscribe((data) => (this.film = data as IFilm))
+    );
+
+    this.route.params
+      .pipe(switchMap((params) => this.api.getEntity('films', params['id'])))
+      .subscribe((data) => (this.film = data as IFilm));
+
     this.sub = this.api
       .getEntity('films', this.route.snapshot.params['id'])
       .subscribe((data) => (this.film = data as IFilm));
