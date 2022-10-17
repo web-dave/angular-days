b7093b06fd7b331dc495de6bd65678387ffe2178
web-dave <dave@webdave.de>
2022-10-17 15:57:34 +0200

    async pipe

--- src/app/swapi/films/films.component.ts
+++ src/app/swapi/films/films.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NEVER, Observable, Subscribable, Subscription } from 'rxjs';
 import { ApiService, ISwapiResponse } from '../api.service';
 
 @Component({
@@ -6,24 +7,33 @@ import { ApiService, ISwapiResponse } from '../api.service';
   templateUrl: './films.component.html',
   styleUrls: ['./films.component.scss'],
 })
-export class FilmsComponent implements OnInit {
+export class FilmsComponent {
   // data?: ISwapiResponse;
-  data: ISwapiResponse | undefined;
-  constructor(private api: ApiService) {}
+  // data: ISwapiResponse | undefined;
+  // sub: Subscription = new Subscription();
 
-  ngOnInit(): void {
-    // // Alt
-    // this.api.getList('films').subscribe(
-    //   (list) => (this.data = list),
-    //   // (err) => console.error(err),
-    //   // () => console.info('Done')
-    // );
+  response$: Observable<ISwapiResponse>;
 
-    // neu
-    this.api.getList('films').subscribe({
-      next: (list) => (this.data = list),
-      error: (err) => console.error(err),
-      complete: () => console.info('Done'),
-    });
+  constructor(private api: ApiService) {
+    this.response$ = this.api.getList('films');
   }
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
+
+  // ngOnInit(): void {
+  //   // // Alt
+  //   // this.api.getList('films').subscribe(
+  //   //   (list) => (this.data = list),
+  //   //   // (err) => console.error(err),
+  //   //   // () => console.info('Done')
+  //   // );
+
+  //   // neu
+  //   this.sub = this.api.getList('films').subscribe({
+  //     next: (list) => (this.data = list),
+  //     error: (err) => console.error(err),
+  //     complete: () => console.info('Done'),
+  //   });
+  // }
 }
