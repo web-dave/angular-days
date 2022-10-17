045dcb1f5e94edc1e4f2c78e492992617fd5a55d
web-dave <dave@webdave.de>
2022-10-17 11:13:59 +0200

    top-nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'swapi-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-days-1022';
